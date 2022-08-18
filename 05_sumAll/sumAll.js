9683d531dc08cef11cdbe93c0ec249c917e5ebc7
BlaqueC88 <99069798+BlaqueC88@users.noreply.github.com>
2022-08-18 08:30:25 +0200

    sumAll & leapYear
    1) sumAll is currently not running, so I can't test it.
    2) leapYears is done.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+  if (isNaN(a) || isNaN(b)) return "ERROR";
+  if (a < 0 || b < 0) return "ERROR";
+  let min = Math.min(a, b);
+  let max = Math.max(a, b);
+  let sum = 0;
+  for (let i = min;i < max; min++);
+      sum = sum + i;
+  return sum;
 };
 
 // Do not edit below this line
